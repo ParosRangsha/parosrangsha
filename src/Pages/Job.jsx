b48fe41d0460bd50066d7d9014db2442108bd847
paros <paroscrangsha@gmail.com>
2025-07-17 11:43:07 +0600

    font change job

--- src/Pages/Job.jsx
+++ src/Pages/Job.jsx
@@ -11,7 +11,7 @@ const Job = () => {
                     <h2 className="text-xl font-semibold text-blue-700">Officer – ERP & Final Inspection</h2>
                     <p className="text-gray-700">Paramount Textile PLC • Bangladesh</p>
                     <p className="text-sm text-gray-500 italic">~5 Years (Currently Working)</p>
-                    <ul className="list-disc list-inside text-gray-700 space-y-1">
+                    <ul className="list-disc list-inside text-gray-700 space-y-1 text-[10px] lg:text-[16px]">
                         <li>Managing ERP data for production and final inspections.</li>
                         <li>Preparing inspection reports and maintaining records.</li>
                         <li>Coordinating with production and planning teams for seamless workflow.</li>
@@ -23,12 +23,12 @@ const Job = () => {
                     <h2 className="text-xl font-semibold text-blue-700">Data Entry Operator</h2>
                     <p className="text-gray-700">Golden Harvest InfoTech Ltd • Bangladesh</p>
                     <p className="text-sm text-gray-500 italic">02 Years</p>
-                    <ul className="list-disc list-inside text-gray-700 mt-2 space-y-1">
-                    <li>Accurate and efficient entry of large data volumes.</li>
-                    <li>Maintaining and organizing digital files and databases.</li>
-                    <li>Performing data quality checks to ensure accuracy.</li>
-                    <li>Supporting team with various back-office tasks.</li>
-                    <li>Working with confidential data while maintaining integrity.</li>
+                    <ul className="list-disc list-inside text-gray-700 mt-2 space-y-1 text-[10px] lg:text-[16px]">
+                        <li>Accurate and efficient entry of large data volumes.</li>
+                        <li>Maintaining and organizing digital files and databases.</li>
+                        <li>Performing data quality checks to ensure accuracy.</li>
+                        <li>Supporting team with various back-office tasks.</li>
+                        <li>Working with confidential data while maintaining integrity.</li>
                     </ul>
                 </div>
             </div>
